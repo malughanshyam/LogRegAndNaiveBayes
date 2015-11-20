c31dac34829a0f0ba9914d64d95b1ae6b8da5cc2
Ghanshyam Malu <malughanshyam@gmail.com>
2015-11-20 17:43:14 -0500

    Logistic Regression and Naive Bayes Modeler

---
+++ LogisticReg.py
@@ -0,0 +1,162 @@
+'''
+Created on Oct 4, 2015
+
+@author  : NiharKhetan, Ghanshyam Malu
+@desc    : Logistic Regression Modeler
+ 
+'''
+from DataParser.ReadCSV import readFileAsVector
+from Bean.Feature import Feature
+from Evaluation.Metrics import *
+import math
+from copy import deepcopy
+
+
+def findSigmoidWX(w,x):
+    ''' Find the sigmoid function '''
+    wx = 0   
+
+    for i in range(len(w)):        
+        wx += float(w[i])*float(x[i])
+    #Using Try to avoid overflow of range; When e to power gets very large it is equivalent to infinity
+    try :
+        e = math.exp(-wx)
+    except :
+        e = 0
+    sigmoid = 1 / float(1 + e)
+    return sigmoid
+          
+def findNorm(vector):
+    ''' Find the normal of the vector'''
+    norm = 0
+    for point in vector:
+        norm += point*point
+    
+    return math.sqrt(norm)
+    
+
+def trainModel(vector):
+    '''
+    Trains the Logistic Regression model
+    '''
+
+    print "\n\tTraining Model .",
+    # Initializing parameters
+    eta = 0.001
+    prec = 5
+    convergenceThreshold = 0.001
+    formatPrintCount = 0
+
+    # Initializing w weight vector to 0 for every feature
+    w = [0]*(len(vector[0])-1)
+    
+    #### Transformations for Training examples and Weight vector
+
+    # Transforming the training examples to canonical representation <1,x1,x2,..,xN>
+    for eachPoint in vector:
+        eachPoint.insert(0,1)
+    
+    # Adding 0 to weight vector at index 0
+    w.insert(0,0)
+    
+    # Repeat until convergence
+    while True:
+        
+        # Save old Weight vector to check for convergence
+        wOld = deepcopy(w)
+
+        #initializing gradient vector    
+        g = [0]*(len(vector[0])-1)
+            
+        for eachPoint in vector[1:]:
+            #pi = 1 /( 1 * exp[ w.xi])
+            pOfi = findSigmoidWX(w, eachPoint[:-1])
+            
+            #error=yi - pi
+            error = int(eachPoint[-1]) - pOfi
+           
+            for j in range(0, len(eachPoint) - 1):
+                g[j] = g[j] + error*int(eachPoint[j])
+            
+            for k in range(0,len(w)):
+                w[k] += eta*g[k]
+                
+        wChange = round(findNorm(w) - findNorm(wOld),prec)
+        
+        if wChange <= convergenceThreshold:
+            break
+        
+        formatPrintCount +=1
+        if formatPrintCount % 100 == 0:
+            print ".",
+            
+    return w
+
+def predictLabelBasedOnThreshold(dataPoint, weightVec):
+    ''' Predicts a class label based on threshold '''
+    
+    thresholdCheck = 0
+    
+    for i in range(len(dataPoint)):
+        thresholdCheck += float(dataPoint[i]) * float(weightVec[i]) 
+    
+    if thresholdCheck < 0.5:
+        return 0
+    else:
+        return 1
+        
+def testModel(vector, weightVec):
+    ''' Test the model '''
+    
+    print "\n\n\tTesting Model .",
+    formatPrintCount = 0
+    
+    predictedLabels = []
+    for dataPoint in vector[1:]:    
+        # Transforming the test datapoints to canonical representation <1,x1,x2,..,xN>
+        dataPoint.insert(0,1)
+        predictedLabels.append(predictLabelBasedOnThreshold(dataPoint[:-1], weightVec))
+        
+        formatPrintCount +=1
+        if formatPrintCount % 10 == 0:
+            print ".",
+            
+    print "\n"
+    return predictedLabels
+
+if __name__ == '__main__':
+    
+    print "=" * 90
+    print "\t\t\tWelcome to Logistic Regression Modeler"
+    print "=" * 90
+
+
+    training_data = "zoo-train.csv"
+    test_data = "zoo-test.csv"
+    
+    trainingVector = readFileAsVector(training_data)
+    testVector = readFileAsVector(test_data)    
+
+    weightVec = trainModel(trainingVector)
+    
+    expectedResults = []
+    
+    for dataPoint in testVector[1:]:
+        expectedResults.append(int(dataPoint[-1]))
+        
+    predictedResults = testModel(testVector,weightVec)
+    
+    
+    ################ Evaluation ############### 
+        
+    #Accuracy
+    findAccuracy(predictedResults, expectedResults)
+    
+    #Confusion Matrix
+    confusionMatrix = constructConfusionMatrix(predictedResults, expectedResults)
+    printConfusionMatrix(confusionMatrix)
+
+    print "\n\n","-" * 90
+    print "\t\tThank you for using the Logistic Regression Modeler"
+    print "-" * 90,"\n\n"
+    
---
+++ NaiveBayes.py
@@ -0,0 +1,163 @@
+'''
+Created on Oct 4, 2015
+
+@author  : NiharKhetan, Ghanshyam Malu
+@desc    : Naive Bayes Modeler
+ 
+'''
+from DataParser.ReadCSV import readFileAsVector
+from Bean.Feature import Feature
+from Evaluation.Metrics import *
+import operator
+
+def convertVectorToColumnar(vector):
+    '''
+    This returns the whole data as a list of features objects
+    currently data is in form [[],[],[]]
+    It converts it to [f0,f1,f2] where each fi is and object
+    @param vector: type(list) vector which is the whole dataset
+    @return columns: type(list) which is the whole data stored as columns
+    '''
+    columns = []
+    columnLabels = vector[0]
+    for column in range(0, len(columnLabels)):
+        columnData = []
+        for row in vector[1:]:
+            columnData.append(row[column])
+        columns.append(Feature(columnLabels[column], columnData))
+    return columns
+
+def computeConditionalProbability(feature, classLabel, laplacianCorrection = False):
+    '''
+    To compute conditional probabilities for a feature given class label
+    '''
+    probDict = {}
+    
+    classLabelData = classLabel.getData()
+    featureData = feature.getData()
+    
+    #compute P(y|x=0), P(y|x=1)... P(y|x=n)
+    for eachClassLabelType in classLabel.getDiscreteSet().keys():        
+        for eachType in feature.getDiscreteSet().keys():
+            # this big enumerate on right gets me all the indexes of eachType in my feature data
+            count = 0
+            for eachIndex in [i for i, x in enumerate(featureData) if x == eachType]:
+                if classLabelData[eachIndex] == eachClassLabelType:
+                    count += 1
+            key = 'P(%s=%s|%s=%s)' % (feature.getName(), eachType, classLabel.getName(), eachClassLabelType)
+            if laplacianCorrection:
+                probDict[key] = (count + 1)/ float((classLabel.getDiscreteSet()[eachClassLabelType] + len(feature.getDiscreteSet().keys())))
+            else:
+                probDict[key] = count / float(classLabel.getDiscreteSet()[eachClassLabelType])
+    
+    return probDict 
+        
+def trainModel(vector):
+    '''
+    Generates a list of conditional probabilities for given setof features against the class label
+    '''
+    
+    print "\n\tTraining Model .",
+    formatPrintCount = 0
+    
+    columnarVector = convertVectorToColumnar(vector)    
+    probList = []
+    
+    for eachFeature in columnarVector:
+        probList.append(computeConditionalProbability(eachFeature, columnarVector[-1], True))
+        if formatPrintCount % 10 == 0:
+            print ".",
+            
+    print "\n"
+   
+    conditionalProbabilityDict = {}
+    
+    for eachDict in probList:
+        for k,v in eachDict.iteritems():
+            conditionalProbabilityDict[k] = v
+        formatPrintCount +=1
+        
+    return conditionalProbabilityDict
+
+def testModel(conditionalProbabilityDict, columnarTrainingVector, columnarTestVector, laplacianCorrection = False):
+    '''
+    gets all the conditional probabilities generated and computes a list of predicted values
+    '''
+    print "\n\tTesting Model .",
+    formatPrintCount = 0
+    
+    priorProbTrue = columnarTrainingVector[-1].getDiscreteSet()['1'] / float(columnarTrainingVector[-1].getDiscreteSet()['1'] + columnarTrainingVector[-1].getDiscreteSet()['0']) 
+    priorProbFalse = columnarTrainingVector[-1].getDiscreteSet()['0'] / float(columnarTrainingVector[-1].getDiscreteSet()['1'] + columnarTrainingVector[-1].getDiscreteSet()['0'])
+    #iterates each row in test data
+    predictedResults = []
+    for i in range(0, len(columnarTestVector[0].getData())):
+        #iterates each classLabel and find likelihood        
+        likelihoodList = {} 
+        for eachClassLabel in columnarTrainingVector[-1].getDiscreteSet().keys():
+            probXGivenY = 1
+            if laplacianCorrection:
+                priorProbGivenLabel = (columnarTrainingVector[-1].getDiscreteSet()[eachClassLabel] + 1)/ float(len(columnarTrainingVector[-1].getData()) + len(columnarTrainingVector[-1].getDiscreteSet().keys()))
+            else:
+                priorProbGivenLabel = columnarTrainingVector[-1].getDiscreteSet()[eachClassLabel] / float(len(columnarTrainingVector[-1].getData()))
+            
+            probXGivenY *= priorProbGivenLabel
+            for eachFeature in columnarTestVector[:-1]:
+                 
+                key = 'P(%s=%s|%s=%s)' % (eachFeature.getName(), eachFeature.getData()[i], columnarTestVector[-1].getName(), eachClassLabel)
+                if key in conditionalProbabilityDict.keys():
+                    probXGivenY *= conditionalProbabilityDict[key]
+                else:                                   
+                    # get laplacian correction for the same feature in training set
+                    lapCorrectionValue = 0
+                    for feature in columnarTrainingVector:
+                        if feature.getName() == eachFeature.getName():                            
+                            lapCorrectionValue = 1 / float(len(feature.getDiscreteSet().keys()))
+                    probXGivenY *= lapCorrectionValue
+            likelihoodList[eachClassLabel] = probXGivenY
+        
+        predictedResults.append(max(likelihoodList.iteritems(), key=operator.itemgetter(1))[0])
+        formatPrintCount +=1
+        if formatPrintCount % 10 == 0:
+            print ".",
+            
+    print "\n"
+    
+    return predictedResults
+
+if __name__ == '__main__':
+    
+    print "=" * 90
+    print "\t\t\tWelcome to Naive Bayes Modeler"
+    print "=" * 90
+
+    #training_data = "BuyCondodataSet.csv"
+    #test_data = "BuyCondodataSetTest.csv"
+    training_data = "zoo-train.csv"
+    test_data = "zoo-test.csv"
+    trainingVector = readFileAsVector(training_data)
+    columnarTrainingVector = convertVectorToColumnar(trainingVector)
+    testVector = readFileAsVector(test_data)    
+    columnarTestVector = convertVectorToColumnar(testVector)
+    
+    
+    condPList = trainModel(trainingVector)
+    expectedResults = columnarTestVector[-1].getData()
+    predictedResults = testModel(condPList, columnarTrainingVector, columnarTestVector, True)
+    
+    ############ Evaluation ########### 
+        
+    #Accuracy
+    findAccuracy(predictedResults, expectedResults)
+    for i in range(0,len(predictedResults)):
+        predictedResults[i] = int(predictedResults[i])
+        expectedResults[i] = int(expectedResults[i])
+
+    #Confusion Matrix
+    confusionMatrix = constructConfusionMatrix(predictedResults, expectedResults)
+    printConfusionMatrix(confusionMatrix)
+    
+    print "\n\n","-" * 90
+    print "\t\tThank you for using the Naive Bayes Modeler"
+    print "-" * 90,"\n\n"
+    
+    
